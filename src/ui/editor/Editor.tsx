44dd1b0c82472161e1cb8dd24d9d6a97296d2b19
Sergey Akishin <morales66@mail.ru>
2026-03-27 14:00:49 +0300

    add title input in editor

--- src/ui/editor/Editor.tsx
+++ src/ui/editor/Editor.tsx
@@ -1,6 +1,45 @@
+import { IoIosSave } from "react-icons/io";
+import { MdDelete } from "react-icons/md";
+import styles from './Editor.module.css'
+import { useNotesContext } from "../../contexts/NotesContext";
+import { useEffect, useState, type ChangeEvent } from "react";
 
 export const Editor = () => {
+	const { activeNote } = useNotesContext();
+	const [titleInputValue, setTitleInputValue] = useState<string>('');
+
+	useEffect(() => {
+		if (activeNote) {
+			setTitleInputValue(activeNote.title)
+		} else {
+			setTitleInputValue('')
+		}
+
+	}, [activeNote])
+
+	const handleTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setTitleInputValue(event.target.value)
+	}
+
+	if (activeNote === null) {
+		return (
+			<div className={styles.empty}>
+				<div>Выберите запись или создайте новую</div>
+			</div>
+		)
+	}
+
 	return (
-		<div>Editor</div>
+		<div className={styles.container}>
+			<div className={styles.header}>
+				<input
+					className={styles.title}
+					placeholder='Введите заголовок записи...'
+					value={titleInputValue}
+					onChange={handleTitleChange}
+				/>
+			</div>
+			<div className={styles.area}></div>
+		</div>
 	)
 }
