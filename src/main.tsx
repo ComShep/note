ea87cd7a7f7d980669a53790676a9792911b98a7
Sergey Akishin <morales66@mail.ru>
2026-05-22 13:07:55 +0300

    test edit readme

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { MainPage } from './MainPage.tsx'
 
-
 createRoot(document.getElementById('root')!).render(
-	// <StrictMode>
-	<MainPage/>
-	// </StrictMode>,
+	<StrictMode>
+		<MainPage/>
+	</StrictMode>,
 )
 
 
